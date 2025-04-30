17eef01f0f92aa40b851725e87883aeca245aae0
NotLarios <andreslariosfajardo@gmail.com>
2025-04-30 02:14:40 -0600

    feat(locust): added 10k object array

---
+++ Proyecto2/locust/index.py
@@ -0,0 +1,43 @@
+import json
+from random import randrange
+from locust import HttpUser, between, task
+
+class ReadFile():
+    def __init__(self):
+        self.data = []    
+    
+    def getData(self):
+        size = len(self.data)
+        if size > 0:
+            index = randrange(0, size - 1) if size > 1 else 0
+            return self.data.pop(index)
+        else:
+            print("No hay más datos para enviar.")
+            return None
+    
+    def loadFile(self):
+        try:
+            with open("./traffic.json", "r", encoding="utf-8") as file:
+                self.data = json.loads(file.read())
+        except Exception as e:
+            print(f'Error: {e}')
+
+
+class trafficData(HttpUser):
+    wait_time = between(0.1, 0.9)
+    reader = ReadFile()
+    reader.loadFile()
+
+    def on_start(self):
+        print("On Start")
+    
+    @task
+    def sendMessage(self):
+        data = self.reader.getData()
+        if data is not None:
+            res = self.client.post("", json=data)
+            response = res.json()
+            print(response)
+        else:
+            print("Empty")
+            self.stop(True)
